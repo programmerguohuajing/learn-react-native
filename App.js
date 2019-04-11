61f038ced5b5bca8650b8b9921edb4cd79cf5b4d
jingguohua <guohuajing@163.com>
2019-04-11 08:41:13 +0800

    change

--- App.js
+++ App.js
@@ -7,7 +7,14 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, PixelRatio} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  PixelRatio,
+  ListView
+} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,6 +26,10 @@ const instructions = Platform.select({
 
 // type Props = {};
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <View>
@@ -29,41 +40,5 @@ export default class App extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 200,
-    marginLeft: 5,
-    marginRight:5,
-    height:5,
-    flexDirection: 'row',
-    borderRadius: 5,
-    padding: 5,
-    backgroundColor: '#FF0067',
-  },
-  item: {
-    flex:1,
-    borderWidth:2,
-    height:80
-  },
-  center: {
-    justifyContent:'center',
-    alignItems: 'center',
-  },
-  flex: {
-    flex:1,
-    backgroundColor: '#FF0067',
-  },
-  font: {
-    color:'#fff',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  lineLeftRight: {
-    borderLeftWidth: 1/PixelRatio.get(),
-    borderRightWidth: 1/PixelRatio.get(),
-    borderColor: '#fff',
-  },
-  lineCenter: {
-    borderBottomWidth:1/PixelRatio.get(),
-    borderColor: '#fff',
-  }
+  flex: 1,
 });
