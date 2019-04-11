15e8a4ba2850a32de055c1aae8d799111742416b
jingguohua <guohuajing@163.com>
2019-04-11 10:49:24 +0800

    'View'

--- App.js
+++ App.js
@@ -13,7 +13,6 @@ import {
   Text,
   View,
   PixelRatio,
-  ListView
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -26,19 +25,71 @@ const instructions = Platform.select({
 
 // type Props = {};
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-      <View>
-        <Text>123</Text>
-      </View>
+        <View style={styles.flex}>
+            <View style={styles.container}>
+                <View style={[styles.item,styles.center]}>
+                    <Text style={styles.font}>酒店</Text>
+                </View>
+                <View style={[styles.item,styles.lineLsftRight]}>
+                    <View style={[styles.center,styles.flex,styles.lineCenter]}>
+                        <Text style={styles.font}>海外酒店</Text>
+                    </View>
+                    <View style={[styles.flex,styles.center]}>
+                        <Text style={styles.font}>特惠酒店</Text>
+                    </View>
+                </View>
+                <View style={styles.item}>
+                    <View style={[styles.flex,styles.center,styles.lineCenter]}>
+                        <Text style={styles.font}>团购</Text>
+                    </View>
+                    <View style={[styles.flex,styles.center]}>
+                        <Text style={styles.font}>客栈/公寓</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  flex: 1,
+    container:{
+        matginTop: 200,
+        marginLeft: 5,
+        matrginRight:5,
+        height: 84,
+        flexDirection: 'row',
+        borderRadius:5,
+        padding:2,
+        backgroundColor: '#FF0067',
+    },
+    item:{
+        flex: 1,
+        height: 80,
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    flex: {
+        flex: 1
+    },
+    font: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    lineLeftRight: {
+        borderLeftWidth: 1/PixelRatio.get(),
+        borderRightWidth: 1/PixelRatio.get(),
+        borderColor: '#fff',
+    },
+    lineCenter:{
+        borderBottomWidth: 1/PixelRatio.get(),
+        borderColor:'#fff',
+    },
 });
+
+// AppRegistry.registerComponent('App', () => App)
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # learn-react-native
+####缘由
+一直做web端的项目 最近新项目中有移动端项目 所以准备做些RN的技术储备
+###react-native学习之View组件
+在React Native里有一个类似于div的组件，那就是View组件，支持多层嵌套，支持flexbox布局
+
+实例步骤：
+
+1.加载View组件
+
+2.创建组件
+
+3.添加样式表
+
+4.注册入口
+
+5.外层布局
+```
+<View style={styles.container}>
+    <View style={styles.item}></View>
+    <View style={styles.item}></View>
+    <View style={styles.item}></View>
+</View>
+```
